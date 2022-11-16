5fd29b8b6a2da249d2933fc040d6bd981086834c
Alvaro Lopez <alvarolopezpineda1999@gmail.com>
2022-11-16 19:04:28 +0100

    primer commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,22 @@
+import { MoviesGrid } from './MoviesGrid';
+import styles from './App.module.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <Router>
+      <header>
+        {/* <h1 className={styles.title}>Movies</h1> */}
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='/movie'>Movie</Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/movie' element={'movie'}></Route>
+          <Route path='/' element={'Home'}></Route>
+        </Routes>
+      </main>
+    </Router>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import { App } from './Components/App.jsx';
+import './index.css';
+const app = <h1>Desarrollo Útil</h1>;
+const container = document.getElementById('root');
+
+ReactDOM.render(<App />, container);
